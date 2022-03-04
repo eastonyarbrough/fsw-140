fbb03880f421f9151524323f04835bf7a31764c1
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2022-03-03 22:09:00 -0600

    Complete 'White Christmas' assignment

--- week6/whiteChristmas/myQueries.sql
+++ week6/whiteChristmas/myQueries.sql
@@ -43,11 +43,20 @@ GROUP BY(yr)
 -- inclusive assuming they were born at the beginning of the year and were about 1 year old on their first Christmas).
 -- Charles Dickens's wcc was 8.
 -- List all the years and the wcc for children born in each year of the data set. Only show years where the wcc was at least 7.
-SELECT yr, CASE WHEN MIN(m12) < 0 THEN COUNT(yr) ELSE '0' END AS wcc
-FROM temperatures WHERE yr BETWEEN yr AND yr + 11 AND dy BETWEEN 21 and 25
-GROUP BY(yr) ORDER BY(yr) ASC
+SELECT birth_year, COUNT(white_xmas) AS wcc
+FROM (
+	SELECT birth_year, yr + 1 - birth_year AS age,
+	CASE WHEN MIN(m12) < 0 THEN 'White Christmas' END AS white_xmas
+	FROM temperatures
+	CROSS JOIN (SELECT DISTINCT yr AS birth_year FROM temperatures) years
+	WHERE yr BETWEEN birth_year + 2 AND birth_year + 11 AND dy BETWEEN 21 AND 25
+	GROUP BY(birth_year, age) 
+) white_christmases
+GROUP BY(birth_year)
+HAVING COUNT(white_xmas) >= 7
 
 -- Display the average temperatures for August by decade.
-SELECT yr, AVG(m8 / 10) AS august FROM temperatures
-WHERE yr BETWEEN yr AND yr + 9
+SELECT yr / 10 * 10 AS decade, AVG(m8 / 10) AS avg_temp
+FROM temperatures
+WHERE yr BETWEEN yr AND yr / 10 * 10
 GROUP BY(yr) ORDER BY(yr) ASC
